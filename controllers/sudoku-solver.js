96c98f3b1928a52a02823c542665d6566a8b2f78
Tezcatlipoca0000 <tezcatlipoca0000@gmail.com>
2022-08-16 17:06:19 -0500

    dev check reg func

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -25,10 +25,44 @@ class SudokuSolver {
 
   checkColPlacement(puzzleString, row, column, value) {
     // i = col-1 i+=9
+    let answer = true;
+    for (let i = column - 1 ; i < (column + 72); i+=9) {
+      if (puzzleString[i] == value) answer = false;
+    }
+    return answer;
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
+    let reg = 0,
+        I = [0, 1, 2, 9, 10, 11, 18, 19, 20],
+        II = [3, 4, 5, 12, 13, 14, 21, 22, 23],
+        III = [6, 7, 8, 15, 16, 17, 24, 25, 26],
+        IV = [27, 28, 29, 36, 37, 38, 45, 46, 47],
+        V = [30, 31, 32, 39, 41, 41, 48, 49, 50],
+        VI = [33, 34, 35, 42, 43, 44, 51, 52, 53],
+        VII = [54, 55, 56, 63, 64, 65, 72, 73, 74],
+        VIII = [57, 58, 59, 66, 67, 68, 75, 76, 77],
+        IX = [60, 61, 62, 69, 70, 71, 78, 79, 80],
 
+    if (column < 4 && row < 4) {
+      reg = 1;
+    } else if (column > 3 && column < 7 && row < 4) {
+      reg = 2;
+    } else if (column > 6 && row < 4) {
+      reg = 3;
+    } else if (column < 4 && row > 3 && row < 7) {
+      reg = 4;
+    } else if (column > 3 && column < 7 && row > 3 && row < 7) {
+      reg = 5;
+    } else if (column > 6 && row > 3 && row < 7) {
+      reg = 6;
+    } else if (column < 4 && row > 6) {
+      reg = 7;
+    } else if (column > 3 && column < 7 && row > 6) {
+      reg = 8;
+    } else if (column > 6 && row > 6) {
+      reg = 9;
+    }
   }
 
   solve(puzzleString) {
@@ -36,7 +70,9 @@ class SudokuSolver {
     for (let i = 0; puzzleString.length; i++) {
       let row = 0,
           col = 0,
-          rowSol = [];
+          rowSol = [],
+          colSol = [],
+          regSol = [];
 
       if (puzzleString[i] === '.') {
 
@@ -74,7 +110,14 @@ class SudokuSolver {
             rowSol.push(j);
           }
         }
-        console.log(`solutions for row: ${row} col: ${col} --- ${rowSol}`);
+        console.log(`row solutions for row: ${row} col: ${col} --- ${rowSol}`);
+
+        for (let j = 1; j < 10; j++) {
+          if (this.checkColPlacement(puzzleString, row, col, j)) {
+            colSol.push(j);
+          }
+        }
+        console.log(`col solutions for row: ${row} col: ${col} --- ${colSol}`);
 
       }
 
